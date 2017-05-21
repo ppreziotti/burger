5f9b3a9dce7e332067309936bbd4359205866e71
ppreziotti <patpreziotti@gmail.com>
2017-05-21 10:33:56 -0400

    setup, initial commit

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+// Setting up MySQL connection
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	port: 3000,
+	host: 'localhost',
+	user: 'root',
+	password: 'fya7b23w',
+	database: 'burgers_db'
+});
+
+// Making the connection
+connection.connect(function(err) {
+	if (err) {
+		throw err;
+	}
+	console.log("Connected as id " + connection.threadId);
+});
+
+// Making the connection available to the ORM via exports
+module.exports = connection;
---
+++ config/orm.js
@@ -0,0 +1,17 @@
+// Requiring the MySQL database connection
+var connection = require('./connection.js');
+
+var orm = {
+	selectAll: function() {
+
+	},
+	insertOne: function() {
+
+	},
+	updateOne: function() {
+
+	}
+}
+
+// Making the ORM available via exports
+module.exports = orm;
