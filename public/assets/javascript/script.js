236b32968684b5fc4a72f71972c8da4eff7e4ed4
ppreziotti <patpreziotti@gmail.com>
2017-05-28 15:05:37 -0400

    updated validation function

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -2,6 +2,7 @@
 function validateForm() {
 	var input = document.forms["burger-form"]["name"].value.trim();
 	if (input.length > 50) {
+		document.forms["burger-form"]["name"].value = "";
 		alert("Burger name cannot contain more than 50 characters. Sorry, menu space is a hot commodity!");
 		return false;
 	}
